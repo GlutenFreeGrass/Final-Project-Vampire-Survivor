d27df368cd9637b22b570fc66681c6815c21085e
qzjo <jaocampo@s.sfusd.edu>
2024-05-17 15:34:01 -0700

    One Player Button and Quit Button

---
+++ 2d-project-start/start_menu.gd
@@ -0,0 +1,15 @@
+extends CenterContainer
+
+@onready var start_game_button = %StartGameButton
+
+func _ready():
+	pass
+
+func _on_single_player_button_pressed():
+	get_tree().change_scene_to_file("res://survivors_game.tscn")
+
+
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
