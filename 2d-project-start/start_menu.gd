a22e3b68b7a71b9dd7121cc162b475bdfbed8a10
qzjo <jaocampo@s.sfusd.edu>
2024-05-20 15:06:43 -0700

    Added Another Player

--- 2d-project-start/start_menu.gd
+++ 2d-project-start/start_menu.gd
@@ -7,9 +7,12 @@ func _ready():
 
 func _on_single_player_button_pressed():
 	get_tree().change_scene_to_file("res://survivors_game.tscn")
-
-
-
+	
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_two_player_button_pressed():
+	print("wow you have friends?")
+	
